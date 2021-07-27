3283d8c0109bbac168f26e445a504f989f023b47
Charly <charly5bollinger@gmail.com>
2021-07-27 11:31:41 +0200

    Initial commit

---
+++ src/main/java/com/github/charlyb01/xpstorage_trinkets/XpstorageTrinkets.java
@@ -0,0 +1,10 @@
+package com.github.charlyb01.xpstorage_trinkets;
+
+import net.fabricmc.api.ModInitializer;
+
+public class XpstorageTrinkets implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/github/charlyb01/xpstorage_trinkets/client/XpstorageTrinketsClient.java
@@ -0,0 +1,13 @@
+package com.github.charlyb01.xpstorage_trinkets.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class XpstorageTrinketsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
