56e1e91918e795a2ca807a1e0f1f654ed4d60f34
Charly <charly5bollinger@gmail.com>
2021-07-27 16:29:26 +0200

    Added items
    Added gem, amulet and ring basic files

--- src/main/java/com/github/charlyb01/xpstorage_trinkets/XpstorageTrinkets.java
+++ src/main/java/com/github/charlyb01/xpstorage_trinkets/XpstorageTrinkets.java
@@ -1,10 +1,22 @@
 package com.github.charlyb01.xpstorage_trinkets;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class XpstorageTrinkets implements ModInitializer {
+    public static final Item.Settings settings = new Item.Settings().group(ItemGroup.MISC);
+
+    public static final Item cristalisLazuli = new Item(settings);
+    public static final Item xp_saver = new Item(settings);
+    public static final Item xp_conduit = new Item(settings);
+
     @Override
     public void onInitialize() {
-
+        Registry.register(Registry.ITEM, new Identifier("xp_storage_trinkets", "cristalis_lazuli"), cristalisLazuli);
+        Registry.register(Registry.ITEM, new Identifier("xp_storage_trinkets", "xp_saver"), xp_saver);
+        Registry.register(Registry.ITEM, new Identifier("xp_storage_trinkets", "xp_conduit"), xp_conduit);
     }
 }
