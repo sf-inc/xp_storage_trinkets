100fac8318171ed6a7d378051ab838a868cf387c
Charly <charly5bollinger@gmail.com>
2021-07-28 17:29:13 +0200

    Made items trinketsitem placable on trinkets slots
    XP Saver and Conduit are now TrinketItem, that can be placed on necklace or ring slots

--- src/main/java/com/github/charlyb01/xpstorage_trinkets/XpstorageTrinkets.java
+++ src/main/java/com/github/charlyb01/xpstorage_trinkets/XpstorageTrinkets.java
@@ -1,5 +1,6 @@
 package com.github.charlyb01.xpstorage_trinkets;
 
+import dev.emi.trinkets.api.TrinketItem;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -10,8 +11,8 @@ public class XpstorageTrinkets implements ModInitializer {
     public static final Item.Settings settings = new Item.Settings().group(ItemGroup.MISC);
 
     public static final Item cristalisLazuli = new Item(settings);
-    public static final Item xp_saver = new Item(settings.maxDamage(1));
-    public static final Item xp_conduit = new Item(settings.maxDamage(1000));
+    public static final Item xp_saver = new TrinketItem(settings.maxDamage(1));
+    public static final Item xp_conduit = new TrinketItem(settings.maxDamage(1000));
 
     @Override
     public void onInitialize() {
